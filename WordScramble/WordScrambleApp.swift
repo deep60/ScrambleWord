e2156c7527248c89d9659fb4720fc823a6ef0d8b
P Deepanshu <97436890+deep60@users.noreply.github.com>
2022-12-31 08:25:01 +0530

    Add files via upload

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by P Deepanshu on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
